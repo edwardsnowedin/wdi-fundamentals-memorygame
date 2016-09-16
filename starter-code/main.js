09b7413030a42684fffb5a9c4205c1ced4fdaeee
Matt <paz.matthew@me.com>
2016-09-16 13:49:12 -0400

    add function in js

--- starter-code/main.js
+++ starter-code/main.js
@@ -8,4 +8,18 @@ if(cardOne === cardTwo){
   alert("You have a match!");
 } else{
   alert('Sorry, try again');
-}
+};
+
+var game = document.getElementById('game-board');
+
+function createBoard () {
+
+  for(var i = 0; i < cards.length; i++){
+  // create div element
+  var cardEl = document.createElement('div');
+  //add a class to the card element
+  cardEl.className = 'card';
+  //append the card to the board
+  board.appendChild(cardElement);
+  }
+};
